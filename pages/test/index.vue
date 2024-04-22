c74eb8915ff4f8f5ec64da28a235fe1bb48689df
y0sh1ma <yoshima@diverta.co.jp>
2024-04-22 21:13:59 +0900

    20240422-3

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -21,7 +21,7 @@
 <script>
 export default {
   async asyncData({ $axios }) {
-    return { response: await $axios.$get('/rcms-api/3/service/4') };
+    return { response: await $axios.$get('/rcms-api/3/service/795') };
   },
 };
 </script>
