04f5815086478d37b942e90172ca20d3795e19a2
y0sh1ma <yoshima@diverta.co.jp>
2025-07-24 18:53:59 +0900

    20250724-5

---
+++ pages/formsample/index.vue
@@ -0,0 +1,21 @@
+﻿
+
+<template>
+  <div>
+
+    <div v-for="n in resp.list" :key="n.slag" >
+      <div>{{ n.subject }}</div>
+      <div>{{ n.contents }}</div>
+      <div>{{ n.topics_id }}</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    return { resp: await $axios.$get('/rcms-api/10/list') };
+  },
+};
+</script>
