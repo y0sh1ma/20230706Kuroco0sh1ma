2e7cbccd19e155d4ab48eee9261a9c74f180872c
y0sh1ma <sanagiy@gmail.com>
2025-07-27 15:28:28 +0900

    20250727-12

--- pages/formsampleditail/index.vue
+++ pages/formsampleditail/index.vue
@@ -35,7 +35,13 @@
 export default {
   async asyncData({ $axios, query }) {
     const id = query.id;
-    const resp = await $axios.$get(`/rcms-api/10/topics/${id}`);
+    const resp = await $axios.$get(`/rcms-api/10/topics/${id}`,
+	        {
+          headers: {
+            'X-RCMS-API-ACCESS-TOKEN': '42275dcbd8c48d924ff658cef0f25d1ac18985ea4f09f3caf9936c509c6db132'
+          }
+        }
+	);
     return { resp };
   },
   methods: {
