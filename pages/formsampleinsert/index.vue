5b7db9fb7076648485ec44db1af2a86fbf52dd2b
y0sh1ma <sanagiy@gmail.com>
2025-07-27 15:48:12 +0900

    20250727-31

--- pages/formsampleinsert/index.vue
+++ pages/formsampleinsert/index.vue
@@ -4,6 +4,12 @@
   >
     <form @submit.prevent="handleSubmit">
       <div style="margin-bottom:16px;">
+        <strong>ID：</strong>
+        <!-- 編集不可なのでspanで表示 -->
+        <span>{{ form.topics_id }}</span>
+      </div>
+
+      <div style="margin-bottom:16px;">
         <strong>タイトル：</strong>
         <input
           v-model="form.subject"
@@ -26,41 +32,64 @@
         <button type="submit" class="btn-primary">保存</button>
         <button type="button" @click="goBack" class="btn-primary">戻る</button>
       </div>
+
+      </div>
+
     </form>
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData({ $axios, query }) {
+    const id = query.id;
+    if (!id) {
+      return { resp: { details: {} } };
+    }
+    const resp = await $axios.$get(`/rcms-api/10/topics/${id}`);
+    return { resp };
+  },
   data() {
     return {
       form: {
+        topics_id: '',
         subject: '',
+        update_ymdhi: '',
         contents: '',
-      }
+      },
+    };
+  },
+  created() {
+    const d = this.resp?.details || {};
+    this.form = {
+	  open_flg: '1',
+      subject: d.subject || '',
+      contents: d.contents || '',
     };
   },
   methods: {
     async handleSubmit() {
       try {
-        await this.$axios.$post(
-          '/rcms-api/10/insert',
-          this.form,
-          {
-            headers: {
-              'X-RCMS-API-ACCESS-TOKEN': '03dc65148cbdad0e9123d14b407282e57c0fee7b69859bf17b16d6e883f0cbb8'
-            }
+        await this.$axios.$post('/rcms-api/10/insert', this.form,
+        {
+          headers: {
+            'X-RCMS-API-ACCESS-TOKEN': '03dc65148cbdad0e9123d14b407282e57c0fee7b69859bf17b16d6e883f0cbb8'
           }
-        );
+        }
+		
+		);
         alert('保存しました（更新日時は自動セットされました）');
       } catch (e) {
         alert('保存に失敗しました');
         console.error(e);
       }
     },
+
     goBack() {
       window.history.back();
-    }
-  }
+    },
+
+  },
 };
 </script>
+
